40b353e29d0bfbb847ba0065c734731e42195e1f
Askeladd <1351914167@qq.com>
2025-08-15 13:46:30 +0800

    update genenrating procedure

--- cmd/llpyg/llpyg.go
+++ cmd/llpyg/llpyg.go
@@ -2,68 +2,69 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
-	"go/token"
-	"log"
-	"strconv"
 	"go/ast"
+	"go/token"
 	"go/types"
+	"log"
+	"os"
 	"os/exec"
-	"strings"
-	"encoding/json"
 	"path/filepath"
+	"strconv"
+	"strings"
+
 	"github.com/goplus/gogen"
+	_ "github.com/goplus/lib/py"
+	"github.com/goplus/llpyg/tool/pyassets"
+	"github.com/goplus/llpyg/tool/pydyn"
 	"github.com/goplus/llpyg/tool/pyenv"
 	"github.com/goplus/llpyg/tool/pysig"
-	_ "github.com/goplus/lib/py"
 )
 
-
 type Config struct {
-	Name 		string 		`json:"name"`			// go module name
-	LibName 	string 		`json:"libName"`		// Python library name
-	LibVersion 	string 		`json:"libVersion"`		// Python library version
-	Modules		[]string 	`json:"modules"`		// Python modules
+	Name       string   `json:"name"`       // go module name
+	LibName    string   `json:"libName"`    // Python library name
+	LibVersion string   `json:"libVersion"` // Python library version
+	Modules    []string `json:"modules"`    // Python modules
 }
 
-
 type symbol struct {
-	Name string `json:"name"`		// python name
+	Name string `json:"name"` // python name
 	Type string `json:"type"`
 	Doc  string `json:"doc"`
 	Sig  string `json:"sig"`
 }
 
 type module struct {
-	Name  string    `json:"name"`		// python module name
+	Name  string    `json:"name"` // python module name
 	Items []*symbol `json:"items"`
 }
 
 type library struct {
-	LibName 	string 		`json:"libName"`
-	Depth 		int  		`json:"depth"`
-	Modules 	[]string 	`json:"modules"`
+	LibName string   `json:"libName"`
+	Depth   int      `json:"depth"`
+	Modules []string `json:"modules"`
 }
 
 func pydump(moduleName string) (mod module, err error) {
-    var out bytes.Buffer
-    cmd := exec.Command("pydump", moduleName)
-    cmd.Stdout = &out
-    cmd.Stderr = os.Stderr
-    err = cmd.Run()
-    if err != nil {
-        return mod, fmt.Errorf("pydump %s failed", moduleName)
-    }
-    err = json.Unmarshal(out.Bytes(), &mod)
-    if err != nil {
-        return mod, fmt.Errorf("unmarshal %s failed", moduleName)
-    }
+	var out bytes.Buffer
+	cmd := exec.Command("pydump", moduleName)
+	cmd.Stdout = &out
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		return mod, fmt.Errorf("pydump %s failed", moduleName)
+	}
+	err = json.Unmarshal(out.Bytes(), &mod)
+	if err != nil {
+		return mod, fmt.Errorf("unmarshal %s failed", moduleName)
+	}
 	if mod.Name != moduleName {
 		return mod, fmt.Errorf("import module failed: %s", moduleName)
 	}
-    return mod, nil
+	return mod, nil
 }
 
 func pymodule(libName string, depth int) (lib library, err error) {
@@ -85,9 +86,36 @@ func pymodule(libName string, depth int) (lib library, err error) {
 	return lib, nil
 }
 
-
-
 func main() {
+
+	// pyHome := pydyn.GetPyHome("/Users/mac/xgo/llpyg/tool/pyassets/python")
+	// fmt.Printf("pyHome: %s\n", pyHome)
+	// if err := pydyn.ApplyEnv(pyHome); err != nil {
+	// 	log.Fatalf("set py env failed: %v\n", err)
+	// }
+	root := os.Getenv("LLGO_ROOT")
+	if root == "" {
+		var err error
+		root, err = os.Getwd()
+		if err != nil {
+			log.Fatalf("failed to get working dir: %v", err)
+		}
+	}
+	pyHome := pydyn.GetPyHome("")
+	if pyHome == "" {
+		var err error
+		// 从二进制内置资产解压一套可用的 Python 到临时目录
+		pyHome, err = pyassets.ExtractToDir(root)
+		if err != nil {
+			log.Fatalf("extract embedded python failed: %v\n", err)
+		}
+	}
+	fmt.Printf("pyHome: %s\n", pyHome)
+	if err := pydyn.ApplyEnv(pyHome); err != nil {
+		log.Fatalf("set py env failed: %v\n", err)
+	}
+	// python library name and version, main module name
+	// e.g. scikit-learn==1.0.2 : scikit-learn, 1.0.2, sklearn
 	var libName, libVersion string
 
 	output := flag.String("o", "./test", "Output dir")
@@ -95,10 +123,10 @@ func main() {
 	modDepth := flag.Int("d", 1, "Extract module depth")
 	flag.Parse()
 
-    if flag.NArg() < 1 {
-        fmt.Fprintln(os.Stderr, "Usage: llpyg [-o outputDir] [-mod modName] [-d modDepth] pythonLibName[==version]")
-        os.Exit(1)
-    }
+	if flag.NArg() < 1 {
+		fmt.Fprintln(os.Stderr, "Usage: llpyg [-o outputDir] [-mod modName] [-d modDepth] pythonLibName[==version]")
+		os.Exit(1)
+	}
 	libArg := flag.Arg(0)
 	libName, libVersion = getNameAndVersion(libArg)
 	if libName == "" {
@@ -122,10 +150,10 @@ func main() {
 		os.Exit(1)
 	}
 	cfg := Config{
-		Name: 		lib.Modules[0],		// go package name
-		LibName: 	libName,
+		Name:       lib.Modules[0], // go package name
+		LibName:    libName,
 		LibVersion: version,
-		Modules: 	lib.Modules,
+		Modules:    lib.Modules,
 	}
 	outDir := filepath.Join(*output, cfg.Name)
 	err = os.RemoveAll(outDir)
@@ -137,10 +165,10 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	
+
 	// go module
 	if *modName == "" {
-		*modName = cfg.Name
+		*modName = "github.com/Bigdata-shiyang/test/" + cfg.Name
 	}
 	genGoModule(*modName, outDir)
 
@@ -159,7 +187,6 @@ func generateFromConfig(cfg Config) {
 	}
 }
 
-
 func genGoBindings(moduleName string) {
 	// pydump
 	mod, err := pydump(moduleName)
@@ -174,18 +201,18 @@ func genGoBindings(moduleName string) {
 	pkg := gogen.NewPackage("", pkgName, nil)
 	pkg.Import("unsafe").MarkForceUsed(pkg)      // import _ "unsafe"
 	py := pkg.Import("github.com/goplus/lib/py") // import "github.com/goplus/lib/py"
-	
+
 	f := func(cb *gogen.CodeBuilder) int {
 		cb.Val("py." + mod.Name)
 		return 1
 	}
 	defs := pkg.NewConstDefs(pkg.Types.Scope())
-	defs.New(f, 0, 0, nil, "LLGoPackage")		// const LLGoPackage = "py.moduleName"
+	defs.New(f, 0, 0, nil, "LLGoPackage") // const LLGoPackage = "py.moduleName"
 
 	obj := py.Ref("Object").(*types.TypeName).Type().(*types.Named)
 	objPtr := types.NewPointer(obj)
-	ret := types.NewTuple(pkg.NewParam(0, "", objPtr))   // return *py.Object
-	
+	ret := types.NewTuple(pkg.NewParam(0, "", objPtr)) // return *py.Object
+
 	// add context
 	ctx := &context{pkg, obj, objPtr, ret, nil, nil, py}
 	ctx.genMod(pkg, &mod)
@@ -195,30 +222,28 @@ func genGoBindings(moduleName string) {
 	}
 
 	// write to file
-    outputFile := moduleToPath(mod.Name)
-    file, err := createFileWithDirs(outputFile)
-    if err != nil {
-        log.Printf("error: failed to create output file %s: %v\n", outputFile, err)
-        return
-    }
-    defer file.Close()
+	outputFile := moduleToPath(mod.Name)
+	file, err := createFileWithDirs(outputFile)
+	if err != nil {
+		log.Printf("error: failed to create output file %s: %v\n", outputFile, err)
+		return
+	}
+	defer file.Close()
 
-    err = pkg.WriteTo(file)
-    if err != nil {
-        log.Printf("error: failed to write Go code to file %s: %v\n", outputFile, err)
-        return
-    }
+	err = pkg.WriteTo(file)
+	if err != nil {
+		log.Printf("error: failed to write Go code to file %s: %v\n", outputFile, err)
+		return
+	}
 }
-
 
 func moduleToPath(moduleName string) string {
-    parts := strings.Split(moduleName, ".")
+	parts := strings.Split(moduleName, ".")
 	fileName := parts[len(parts)-1] + ".go"
-    parts = parts[1:]
+	parts = parts[1:]
 	path := strings.Join(parts, "/")
-    return filepath.Join(path, fileName)
+	return filepath.Join(path, fileName)
 }
-
 
 type context struct {
 	pkg    *gogen.Package
@@ -231,15 +256,14 @@ type context struct {
 }
 
 type element struct {
-	name 	string
-	pyType 	string
+	name   string
+	pyType string
 }
 
-
-var funcSet = []string {
+var funcSet = []string{
 	"ufunc",
 	"method",
-	"function", 
+	"function",
 	"method-wrapper",
 	"builtin_function_or_method",
 	"_ArrayFunctionDispatcher",
@@ -274,14 +298,14 @@ func (ctx *context) genFunc(pkg *gogen.Package, sym *symbol) {
 	if len(name) == 0 || name[0] == '_' {
 		return
 	}
-	if symSig == "" {		// no signature
+	if symSig == "" { // no signature
 		ctx.skips = append(ctx.skips, element{name: name, pyType: sym.Type})
 		return
 	}
 	// signature
 	params, variadic := ctx.genParams(pkg, symSig)
 	name = genName(name, -1)
-	sig := types.NewSignatureType(nil, nil, nil, params, ctx.ret, variadic)		// ret: *py.Object
+	sig := types.NewSignatureType(nil, nil, nil, params, ctx.ret, variadic) // ret: *py.Object
 	fn := pkg.NewFuncDecl(token.NoPos, name, sig)
 	// doc
 	docList := ctx.genDoc(sym.Doc)
@@ -302,8 +326,8 @@ func (ctx *context) genParams(pkg *gogen.Package, sig string) (*types.Tuple, boo
 	list := make([]*types.Var, 0, n)
 	for i := 0; i < n; i++ {
 		name := args[i].Name
-		name = strings.NewReplacer("[", "", "]", "").Replace(name)		// ([start,] stop[, step,], dtype=None, *, device=None, like=None)
-		name = strings.NewReplacer("(", "", ")", "").Replace(name)		// ( (a1, a2, ...), axis=0, out=None, dtype=None, casting=\"same_kind\" )
+		name = strings.NewReplacer("[", "", "]", "").Replace(name) // ([start,] stop[, step,], dtype=None, *, device=None, like=None)
+		name = strings.NewReplacer("(", "", ")", "").Replace(name) // ( (a1, a2, ...), axis=0, out=None, dtype=None, casting=\"same_kind\" )
 		name = strings.ReplaceAll(name, ".", "")
 		name = strings.TrimSpace(name)
 		if name == "/" || name == "" || name == "," {
@@ -324,17 +348,16 @@ func (ctx *context) genParams(pkg *gogen.Package, sig string) (*types.Tuple, boo
 	return types.NewTuple(list...), false
 }
 
-
 // round round_
 func genName(name string, idxDontTitle int) string {
 
-    lastIdx := len(name) - 1
-    for lastIdx >= 0 && name[lastIdx] == '_' {
-        lastIdx--
-    }
+	lastIdx := len(name) - 1
+	for lastIdx >= 0 && name[lastIdx] == '_' {
+		lastIdx--
+	}
 	workingName := name[:lastIdx+1]
 	trail := name[lastIdx+1:]
-    parts := strings.Split(workingName, "_")
+	parts := strings.Split(workingName, "_")
 
 	for i, part := range parts {
 		if i != idxDontTitle && part != "" {
@@ -356,7 +379,6 @@ func (ctx *context) genLinkname(name string, sym *symbol) *ast.Comment {
 	return &ast.Comment{Text: "//go:linkname " + name + " py." + sym.Name}
 }
 
-
 // Generate documentation comments from the symbol's doc string
 func (ctx *context) genDoc(doc string) []*ast.Comment {
 	if doc == "" {
@@ -370,15 +392,12 @@ func (ctx *context) genDoc(doc string) []*ast.Comment {
 	return list
 }
 
-
-
 const (
 	NameValist = "__llgo_va_list"
 )
 
 var (
 	emptyCommentLine = &ast.Comment{Text: "//"}
-	tyAny = types.NewInterfaceType(nil, nil)
-	vArgs = types.NewParam(0, nil, NameValist, types.NewSlice(tyAny))
+	tyAny            = types.NewInterfaceType(nil, nil)
+	vArgs            = types.NewParam(0, nil, NameValist, types.NewSlice(tyAny))
 )
-
