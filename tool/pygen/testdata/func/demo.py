91b81836e5d490b98db976ef52a7cafc685b635d
Micro Liu <liuxiangfu2002@163.com>
2025-09-25 15:39:41 +0800

    test: add gen func test

---
+++ tool/pygen/testdata/func/demo.py
@@ -0,0 +1,19 @@
+# demo for function
+
+def func_a():
+    pass
+
+def func_b() -> int:
+    pass
+
+def func_c(a: int, b: float) -> str:
+    pass
+
+def func_d(a: int = 1, b: float = 2.0):
+    pass
+
+def func_e(start, /, unit: 'str | None' = None):
+    pass
+
+def _func_f(a, b):
+    pass
